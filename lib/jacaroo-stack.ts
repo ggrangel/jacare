d21e19feb0ab93dc00b832561fe5a67e7dfa01cc
Gustavo Rangel <gustavorangel91@gmail.com>
2024-05-29 20:58:09 -0300

    Tweak some CDK config

--- lib/jacaroo-stack.ts
+++ lib/jacaroo-stack.ts
@@ -1,16 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3 } from "aws-cdk-lib";
 
-export class JacarooStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class JacarooStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'JacarooQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+    });
   }
 }
